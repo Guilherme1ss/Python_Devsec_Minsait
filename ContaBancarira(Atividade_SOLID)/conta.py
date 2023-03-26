6fac7673d8a9839bda7bfcc77de387fba613cc67
Guilherme Ferreira <49596353+Guilherme1ss@users.noreply.github.com>
2023-03-26 05:10:35 -0300

    Add contaBancaria (Atividade SOLID)

---
+++ ContaBancarira(Atividade_SOLID)/conta.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+
+class Conta(ABC):
+    def __init__(self, id_conta: int, saldo: float):
+        self.id_conta = id_conta
+        self._saldo = saldo
+    
+    @property
+    def saldo(self) -> float:
+        return self._saldo
+    
+    @saldo.setter
+    def saldo(self, value: float):
+        self._saldo = value
+    
+    @abstractmethod
+    def sacar(self, valor: float) -> bool:
+        pass
+    
+    @abstractmethod
+    def depositar(self, valor: float) -> None:
+        pass
